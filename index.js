8b8fe013771f052609f535e5abdc94a2a425585a
gurelbs <guri240@gmail.com>
2021-11-12 00:17:44 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'build')));
+  app.get('/', function(req, res) {
+      res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+  
+}
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
