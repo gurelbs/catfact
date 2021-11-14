1447f20379b7c6970c7150831881f3e12a237ac9
gurelbs <guri240@gmail.com>
2021-11-14 02:52:05 +0200

    auto

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,28 +1,44 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MatSidenavModule } from '@angular/material/sidenav';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomepageComponent } from './components/homepage/homepage.component';
 import { HttpClientModule } from '@angular/common/http';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { IvyCarouselModule } from 'angular-responsive-carousel';
+import { LayoutModule } from '@angular/cdk/layout';
+// material
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { CarouselComponent } from './components/carousel/carousel.component';
-import {IvyCarouselModule} from 'angular-responsive-carousel';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
+//  components
+import { AppRoutingModule } from './app-routing.module';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 @NgModule({
-  declarations: [AppComponent, HomepageComponent, CarouselComponent],
+  declarations: [AppComponent, CarouselComponent, NavbarComponent],
   imports: [
+    // App
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    LayoutModule,
+    // Material
     MatSidenavModule,
     MatGridListModule,
     MatCardModule,
-    IvyCarouselModule
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    // Carousel
+    IvyCarouselModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
